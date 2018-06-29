b000ef9fecd3a5f499b8e05b8dde86d2a46fa892
ronald.arias <ronald.arias@udla.edu.ec>
2018-06-29 12:52:55 -0500

    List-customer component y html

--- src/app/components/list-customer/list-customer.component.ts
+++ src/app/components/list-customer/list-customer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../../model/customer';
+import { CustomerService } from '../../services/customer.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-customer',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListCustomerComponent implements OnInit {
 
-  constructor() { }
+  customers: Customer[];
+
+  constructor( private router: Router, private service: CustomerService ) { }
 
   ngOnInit() {
+    this.service.getCustomers()
+      .subscribe(data => {
+        this.customers = data;
+      });
+  }
+
+  deleteCustomer( customer: Customer ): void {
+    this.service.deleteCustomer( customer.id )
+      .subscribe(data => {
+        this.customers = this.customers.filter( c => c !== customer );
+      });
+  }
+
+  editCustomer( customer: Customer ): void {
+    localStorage.removeItem('editCustomerId');
+    localStorage.setItem('editCustomerId', customer.id.toString());
+    this.router.navigate(['edit-customer']);
+  }
+
+  addCustomer(): void {
+    this.router.navigate(['add-customer']);
   }
 
 }
