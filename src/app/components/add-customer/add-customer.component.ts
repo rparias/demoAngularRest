95570fcd8087bc4bced19573bf2eb75f60043e22
ronald.arias <ronald.arias@udla.edu.ec>
2018-06-29 13:35:48 -0500

    Agregar customer funcionando

--- src/app/components/add-customer/add-customer.component.ts
+++ src/app/components/add-customer/add-customer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-add-customer',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCustomerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private service: CustomerService) { }
+
+  addForm: FormGroup;
 
   ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      id: [],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.service.createCustomer( this.addForm.value )
+      .subscribe(data => {
+        this.router.navigate(['list-customer']);
+      });
   }
 
 }
